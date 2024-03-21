4c273c3ad5c07e4732c7b8db8457776464ab513a
Lost.1.Found <126963496+spaarowbot@users.noreply.github.com>
2024-03-20 23:51:46 -0600

    Add files via upload

--- data/connection.php
+++ data/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "lost1found";
-$password = "Tye858k24.";
-$dbname = "lost1found_lost1DB";
+$username = "root";
+$password = "Tye858k24.3008";
+$dbname = "root_spaarowDB";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
